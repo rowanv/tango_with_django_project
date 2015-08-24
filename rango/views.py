7880a5eb94006a1f2a1cbb0bdcba3662cf521393
rvasquez <rvasquez@twitter.com>
2015-08-24 12:42:51 -0500

    Added two templates, serve a static file, configured to receive a static file. End through lesson 5

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-	return HttpResponse("Rango says hey there world! <a href='/rango/about'>About</a>")
+	context_dict = {'boldmessage': 'I am bold font from the context'}
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-	return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+	context_dict = {'boldmessage': 'This is the about page!'}
+	return render(request, 'rango/about.html', context_dict)
