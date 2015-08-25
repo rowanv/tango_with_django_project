949f8f5b0d071ba891ee3678f89d5125af427ddf
rowanv <rrvspam@gmail.com>
2015-08-25 12:06:00 -0500

    Added form for adding categories, form for adding links

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -27,6 +28,47 @@ def category(request, category_name_url):
 		pages = Page.objects.filter(category=category)
 		context_dict['pages'] = pages
 		context_dict['category'] = category
+		context_dict['category_name_slug'] = category.slug
 	except Category.DoesNotExist:
 		pass
 	return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print(form.errors)
+	else:
+		form = CategoryForm()
+
+	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_url):
+
+    try:
+        cat = Category.objects.get(slug=category_name_url)
+    except Category.DoesNotExist:
+                cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_url)
+        else:
+            print(form.errors)
+    else:
+        form = PageForm()
+
+    context_dict = {'form':form, 'category': cat, 'category_name_slug': category_name_url}
+
+    return render(request, 'rango/add_page.html', context_dict)
